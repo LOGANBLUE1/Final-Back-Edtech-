e2a193179d0948b4a5fd7a852334610a367a09ea
aditya <mrpareekaditya@gmail.com>
2024-05-16 10:50:51 +0530

    ofc

--- models/OTP.js
+++ models/OTP.js
@@ -1,19 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
-const otpScmena = new mongoose.Schema({
+const OTPSchema = new mongoose.Schema({
     email:{
         type:String,
-        required:true
+        required: true,
     },
-    otp:{
+    otp: {
         type:String,
-        required:true
+        required:true,
     },
-    createdAt:{
+    createdAt: {
         type:Date,
         default:Date.now(),
-        expires: 5*60 //seconds
+        expires: 5*60,
     }
 });
 
-module.exports = mongoose.model("OTP",otpScmena);
+
+//a function -> to send emails
+async function sendVerificationEmail(email, otp) {
+    try{
+        const mailResponse = await mailSender(email, "Verification Email from StudyNotion", otp);
+        console.log("Email sent Successfully: ", mailResponse);
+    }
+    catch(error) {
+        console.log("error occured while sending mails: ", error);
+        throw error;
+    }
+}
+
+OTPSchema.pre("save", async function(next) {
+    await sendVerificationEmail(this.email, this.otp);
+    next();
+}) 
+
+
+
+module.exports = mongoose.model("OTP", OTPSchema);
+
